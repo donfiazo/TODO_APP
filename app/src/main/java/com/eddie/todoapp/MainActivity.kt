11712be46858b2f27ae1728819ec3d3504d15958
donfiazo <donfiazo@gmail.com>
2021-06-21 15:05:45 +0100

    Initial Commit

---
+++ app/src/main/java/com/eddie/todoapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.eddie.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+    val username : String = "admin"
+    val password : String = "admin"
+
+    private lateinit var usernameInput : EditText
+
+    private lateinit var passwordInput : EditText
+
+    private lateinit var submitButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
