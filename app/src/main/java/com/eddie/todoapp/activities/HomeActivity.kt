092929bb2db5d924d9453ec8170b21e2c005c05e
donfiazo <donfiazo@gmail.com>
2021-06-23 14:55:52 +0100

    new activity

--- app/src/main/java/com/eddie/todoapp/activities/HomeActivity.kt
+++ app/src/main/java/com/eddie/todoapp/activities/HomeActivity.kt
@@ -1,13 +1,16 @@
-package com.eddie.todoapp
+package com.eddie.todoapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Display
+import android.widget.Button
 import android.widget.TextView
+import com.eddie.todoapp.R
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var usernameDisplay: TextView
+    private lateinit var profileButton : Button
 
 
 
@@ -15,7 +18,14 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home2)
         usernameDisplay = findViewById(R.id.usernameDisplay)
+        profileButton = findViewById(R.id.profileButton)
 
         usernameDisplay.text = intent.getStringExtra("username")
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
